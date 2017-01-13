1e8d6075aee6efacafb25a98f6390681ad36f21e
murphypicard <ayaghsizian@gmail.com>
2017-01-13 14:35:02 -0500

    cc on file at heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 // BASE SETUP
 // =============================================================================
-
+// https://ara-beer-api.herokuapp.com/
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
